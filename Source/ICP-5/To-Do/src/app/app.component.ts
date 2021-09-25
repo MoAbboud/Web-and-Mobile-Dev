b62a3054bfbbcc9841a8de909f1f9b58e1c6efbd
MoAbboud <89588118+MoAbboud@users.noreply.github.com>
2021-09-24 22:02:54 -0500

    update code and screenshots for comments

--- Source/ICP-5/To-Do/src/app/app.component.ts
+++ Source/ICP-5/To-Do/src/app/app.component.ts
@@ -7,17 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  //Declare the items array for the tasks
   public items = [];
 
+  //newTask value that will bring the information from the input.
+  //And also be taking back to be displayed to the user.
   public newTask: string | undefined;
 
   title: string | undefined;
+  //Add task to list
   public addToList() {
+    //Only add the task when it is not empty
     if (this.newTask == '') {
 
     }
     else
     {
+      //Add the task to the array.
       // @ts-ignore
       this.items.push(this.newTask);
       this.newTask = '';
@@ -25,6 +31,7 @@ export class AppComponent {
   }
 
   public deleteTask(index: number) {
+    //Delete the task from the array at the specified index.
     this.items.splice(index, 1);
   }
 }
